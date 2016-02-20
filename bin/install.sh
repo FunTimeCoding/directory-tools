202621aab93ffa84e166d26d721b91b8462a6755
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-20 16:42:57 +0100

    fix install password being set

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,6 @@ DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}/../lib/directory_tools.sh"
-sudo sh -c "echo \"slapd/root_password password string ${MANAGER_PASSWORD}\" | debconf-set-selections"
-sudo sh -c "echo \"slapd/root_password_again password string ${MANAGER_PASSWORD}\" | debconf-set-selections"
-sudo sh -c 'DEBIAN_FRONTEND=noninteractive apt-get install slapd'
-sudo apt-get install -qq ldap-utils nmap
+echo "slapd slapd/password1 password ${MANAGER_PASSWORD}" | sudo debconf-set-selections
+echo "slapd slapd/password2 password ${MANAGER_PASSWORD}" | sudo debconf-set-selections
+sudo apt-get install -qq slapd ldap-utils nmap
--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -2,4 +2,5 @@
 
 sudo service slapd stop
 sudo apt-get purge slapd
+sudo rm -rf /etc/ldap
 sudo rm -rf /var/lib/ldap
