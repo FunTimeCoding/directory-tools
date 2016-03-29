bb4de2b69f31a8f37467b9262b5447f88b316ea4
Alexander Reitzel <funtimecoding@gmail.com>
2016-03-29 19:39:35 +0200

    use _PASSWORD commands to authenticate the manager in user.sh and group.sh, add a list command to both

--- bin/group.sh
+++ bin/group.sh
@@ -5,7 +5,7 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 
 usage()
 {
-    echo "Usage: ${0} add|add_user|delete NAME [GROUP_NUMBER]"
+    echo "Usage: ${0} add|add_user|list|delete NAME [GROUP_NUMBER]"
     echo "Example: ${0} add example"
 }
 
@@ -14,45 +14,61 @@ usage()
 VERB="${1}"
 NAME="${2}"
 
-if [ "${NAME}" = "" ]; then
+if [ "${VERB}" = "" ]; then
     usage
 
     exit 1
 fi
 
-GROUP_DN="cn=${NAME},ou=groups,${SUFFIX}"
-
 if [ "${VERB}" = "add" ]; then
-	GROUP_NUMBER="${3}"
+    if [ "${NAME}" = "" ]; then
+        usage
 
-	if [ "${GROUP_NUMBER}" = "" ]; then
-		GROUP_NUMBER_FILE="group_number.txt"
+        exit 1
+    fi
 
-		if [ ! -f "${GROUP_NUMBER_FILE}" ]; then
-			echo "2000" > "${GROUP_NUMBER_FILE}"
-    	fi
+    GROUP_DN="cn=${NAME},ou=groups,${SUFFIX}"
+    GROUP_NUMBER="${3}"
+    LAST_GROUP_NUMBER_FILE="last_group_number.txt"
 
-    	GROUP_NUMBER=$(cat "${GROUP_NUMBER_FILE}")
-	fi
+    if [ "${GROUP_NUMBER}" = "" ]; then
+        if [ -f "${LAST_GROUP_NUMBER_FILE}" ]; then
+            GROUP_NUMBER="2000"
+        else
+            GROUP_NUMBER=$(cat "${LAST_GROUP_NUMBER_FILE}")
+            GROUP_NUMBER=$(echo "${GROUP_NUMBER} + 1" | bc)
+        fi
+    fi
 
-	echo "GROUP_NUMBER: ${GROUP_NUMBER}"
+    echo "GROUP_NUMBER: ${GROUP_NUMBER}"
     echo "dn: ${GROUP_DN}
 objectClass: posixGroup
 cn: ${NAME}
-gidNumber: ${GROUP_NUMBER}" | ${ADD_MANAGER}
-    NEXT_GROUP_NUMBER=$(echo "${GROUP_NUMBER} + 1" | bc)
-    echo "${NEXT_GROUP_NUMBER}" > "${GROUP_NUMBER_FILE}"
+gidNumber: ${GROUP_NUMBER}" | ${ADD_MANAGER_PASSWORD}
+    echo "${GROUP_NUMBER}" > "${LAST_GROUP_NUMBER_FILE}"
 elif [ "${VERB}" = "add_user" ]; then
-	echo "Enter user to add:"
-	read -r USER_NAME
+    if [ "${NAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
+    GROUP_DN="cn=${NAME},ou=groups,${SUFFIX}"
+    echo "Enter user to add:"
+    read -r USER_NAME
     echo "dn: ${GROUP_DN}
 changeType: modify
 add: memberUid
-memberUid: ${USER_NAME}" | ${MODIFY_MANAGER}
+memberUid: ${USER_NAME}" | ${MODIFY_MANAGER_PASSWORD} "${GROUP_DN}"
 elif [ "${VERB}" = "delete" ]; then
-    ${DELETE_MANAGER} "${GROUP_DN}"
-else
-    usage
+    if [ "${NAME}" = "" ]; then
+        usage
 
-    exit 1
+        exit 1
+    fi
+
+    GROUP_DN="cn=${NAME},ou=groups,${SUFFIX}"
+    ${DELETE_MANAGER_PASSWORD} "${GROUP_DN}"
+elif [ "${VERB}" = "list" ]; then
+    ${SEARCH_MANAGER_PASSWORD} -b "ou=groups,${SUFFIX}" "(objectClass=posixGroup)"
 fi
--- bin/user.sh
+++ bin/user.sh
@@ -14,21 +14,29 @@ usage()
 VERB="${1}"
 USERNAME="${2}"
 
-if [ "${VERB}" = "" ] || [ "${USERNAME}" = "" ]; then
+if [ "${VERB}" = "" ]; then
     usage
 
     exit 1
 fi
 
-FIRST_NAME="${FULL_NAME% *}"
-LAST_NAME="${FULL_NAME#* }"
-FIRST_LETTER=$(echo "${FIRST_NAME}" | head -c 1)
-USERNAME=$(echo "${FIRST_LETTER}${LAST_NAME}" | sed 's/.*/\L&/')
-USER_PASSWORD=$(slappasswd -s "${USERNAME}")
-USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
-
 if [ "${VERB}" = "add" ]; then
+    if [ "${USERNAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
     FULL_NAME="${3}"
+    USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
+
+    # TODO: Either make username derived from real name optional or let usernames be nicknames and enforce them with a tight convention.
+    #FIRST_LETTER=$(echo "${FIRST_NAME}" | head -c 1)
+    #USERNAME=$(echo "${FIRST_LETTER}${LAST_NAME}" | sed 's/.*/\L&/')
+
+    FIRST_NAME="${FULL_NAME% *}"
+    LAST_NAME="${FULL_NAME#* }"
+    USER_PASSWORD=$(slappasswd -s "${USERNAME}")
 
     if [ "${FULL_NAME}" = "" ]; then
         usage
@@ -37,15 +45,15 @@ if [ "${VERB}" = "add" ]; then
     fi
 
     USER_NUMBER="${4}"
+    LAST_USER_NUMBER_FILE="last_user_number.txt"
 
     if [ "${USER_NUMBER}" = "" ]; then
-        USER_NUMBER_FILE="user_number.txt"
-
-        if [ ! -f "${USER_NUMBER_FILE}" ]; then
-            echo "2000" > "${USER_NUMBER_FILE}"
+        if [ -f "${LAST_USER_NUMBER_FILE}" ]; then
+            USER_NUMBER=$(cat "${LAST_USER_NUMBER_FILE}")
+            USER_NUMBER=$(echo "${USER_NUMBER} + 1" | bc)
+        else
+            USER_NUMBER="2000"
         fi
-
-        USER_NUMBER=$(cat "${USER_NUMBER_FILE}")
     fi
 
     echo "USER_NUMBER: ${USER_NUMBER}"
@@ -68,23 +76,46 @@ displayName: ${USERNAME}
 mail: ${USERNAME}@${DOMAIN}.${TOP_LEVEL}
 shadowLastChange: 0
 shadowMax: 0
-shadowWarning: 0" | ${ADD_MANAGER}
-    NEXT_USER_NUMBER=$(echo "${USER_NUMBER} + 1" | bc)
-    echo "${NEXT_USER_NUMBER}" > "${USER_NUMBER_FILE}"
+shadowWarning: 0" | ${ADD_MANAGER_PASSWORD}
+    echo "${USER_NUMBER}" > "${LAST_USER_NUMBER_FILE}"
 elif [ "${VERB}" = "test" ]; then
+    if [ "${USERNAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
+    FULL_NAME="${3}"
+    USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
     echo "Who am I?"
     ${WHO} -D "${USER_DN}"
     echo "Self search"
     ${SEARCH} -D "uid=${USERNAME},ou=users,${SUFFIX}" -b "uid=${USERNAME},ou=users,${SUFFIX}"
 elif [ "${VERB}" = "set_password" ]; then
+    if [ "${USERNAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
+    FULL_NAME="${3}"
+    USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
     echo "Enter new password:"
     read -r NEW_PASSWORD
     ENCRYPTED_PASSWORD=$(slappasswd -s "${NEW_PASSWORD}")
     echo "Enter manager password:"
     echo "dn: ${USER_DN}
 replace: userPassword
-userPassword: ${ENCRYPTED_PASSWORD}" | ${MODIFY_MANAGER}
+userPassword: ${ENCRYPTED_PASSWORD}" | ${MODIFY_MANAGER_PASSWORD}
 elif [ "${VERB}" = "change_password" ]; then
+    if [ "${USERNAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
+    FULL_NAME="${3}"
+    USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
     echo "Enter new password:"
     read -r NEW_PASSWORD
     ENCRYPTED_PASSWORD=$(slappasswd -s "${NEW_PASSWORD}")
@@ -93,5 +124,16 @@ elif [ "${VERB}" = "change_password" ]; then
 replace: userPassword
 userPassword: ${ENCRYPTED_PASSWORD}" | ${MODIFY} -D "uid=${USERNAME},ou=users,${SUFFIX}"
 elif [ "${VERB}" = "delete" ]; then
-    ${DELETE_MANAGER} "${USER_DN}"
+    if [ "${USERNAME}" = "" ]; then
+        usage
+
+        exit 1
+    fi
+
+    FULL_NAME="${3}"
+    USER_DN="uid=${USERNAME},ou=users,${SUFFIX}"
+    ${DELETE_MANAGER_PASSWORD} "${USER_DN}"
+    # TODO: Consider decreasing the user and group number if they are the most recent one.
+elif [ "${VERB}" = "list" ]; then
+    ${SEARCH_MANAGER_PASSWORD} -b "ou=users,${SUFFIX}" "(objectClass=inetOrgPerson)"
 fi
