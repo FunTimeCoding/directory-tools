1fd9ccaafc07e2bfc9f8d12c5ddaab4471d83f68
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-10 21:51:18 +0200

    synchronize with skeleton

--- upload-job-config.sh
+++ upload-job-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-~/Code/Personal/jenkins-tools/bin/delete-job.sh directory-tools || true
-~/Code/Personal/jenkins-tools/bin/put-job.sh directory-tools job.xml
+~/src/jenkins-tools/bin/delete-job.sh directory-tools || true
+~/src/jenkins-tools/bin/put-job.sh directory-tools job.xml
