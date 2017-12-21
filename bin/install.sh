98a43cc9d55bf9f7135d08db98c62d0a2ca68c1b
Alexander Reitzel <funtimecoding@gmail.com>
2017-12-21 14:35:37 +0100

    remove install and uninstall scripts after integrating them into ansible and runbook

--- bin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-DIRECTORY=$(dirname "${0}")
-SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
-# shellcheck source=/dev/null
-. "${SCRIPT_DIRECTORY}/../lib/directory-tools.sh"
-echo "slapd slapd/password1 password ${MANAGER_PASSWORD}" | sudo debconf-set-selections
-echo "slapd slapd/password2 password ${MANAGER_PASSWORD}" | sudo debconf-set-selections
-sudo apt-get install -qq slapd ldap-utils nmap
--- bin/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-sudo service slapd stop
-sudo apt-get purge slapd
-sudo rm -rf /etc/ldap
-sudo rm -rf /var/lib/ldap
