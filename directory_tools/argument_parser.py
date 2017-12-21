d2c7b6afce12e978570d04a41a9cb757850344c1
Alexander Reitzel <funtimecoding@gmail.com>
2017-12-21 22:45:01 +0100

    add argument parsing for groups

--- directory_tools/argument_parser.py
+++ directory_tools/argument_parser.py
@@ -11,6 +11,7 @@ class Parser:
         )
         subparsers = self.parser.add_subparsers()
         self.add_user_child_parser(subparsers)
+        self.add_group_child_parser(subparsers)
         self.add_status_child_parser(subparsers)
         self.parsed_arguments = self.parser.parse_args(arguments)
 
@@ -45,15 +46,14 @@ class Parser:
         )
         add_parser.add_argument('add', action='store_true')
 
-        delete_parent = CustomArgumentParser(add_help=False)
-        delete_parent.add_argument('--user-name', required=True)
-        delete_parent.add_argument('--full-name', required=True)
-        delete_parser = user_subparsers.add_parser(
-            'delete',
-            parents=[delete_parent],
-            help='delete a user'
+        remove_parent = CustomArgumentParser(add_help=False)
+        remove_parent.add_argument('--name', required=True)
+        remove_parser = user_subparsers.add_parser(
+            'remove',
+            parents=[remove_parent],
+            help='remove a user'
         )
-        delete_parser.add_argument('delete', action='store_true')
+        remove_parser.add_argument('remove', action='store_true')
 
         show_parent = CustomArgumentParser(add_help=False)
         show_group = show_parent.add_mutually_exclusive_group(required=True)
@@ -66,6 +66,49 @@ class Parser:
         show_parser.add_argument('show', action='store_true')
 
         list_parser = user_subparsers.add_parser('list', help='list all users')
+        list_parser.add_argument('list', action='store_true')
+
+    @staticmethod
+    def add_group_child_parser(subparsers) -> None:
+        group_parent = CustomArgumentParser(add_help=False)
+        group_parser = subparsers.add_parser(
+            'group',
+            parents=[group_parent],
+            help='manage groups'
+        )
+        group_parser.add_argument('group', action='store_true')
+        group_subparsers = group_parser.add_subparsers()
+
+        add_parent = CustomArgumentParser(add_help=False)
+        add_parent.add_argument('--name', required=True)
+        add_parser = group_subparsers.add_parser(
+            'add',
+            parents=[add_parent],
+            help='add a group'
+        )
+        add_parser.add_argument('add', action='store_true')
+
+        remove_parent = CustomArgumentParser(add_help=False)
+        remove_parent.add_argument('--name', required=True)
+        remove_parser = group_subparsers.add_parser(
+            'remove',
+            parents=[remove_parent],
+            help='remove a group'
+        )
+        remove_parser.add_argument('remove', action='store_true')
+
+        show_parent = CustomArgumentParser(add_help=False)
+        show_group = show_parent.add_mutually_exclusive_group(required=True)
+        show_group.add_argument('--name', required=True)
+        show_parser = group_subparsers.add_parser(
+            'show',
+            parents=[show_parent],
+            help='show a group'
+        )
+        show_parser.add_argument('show', action='store_true')
+
+        list_parser = group_subparsers.add_parser('list',
+                                                  help='list all groups')
         list_parser.add_argument('list', action='store_true')
 
     @staticmethod
