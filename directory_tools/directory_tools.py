15af3e7dc69cc590781a138cb03a0908f62ad008
Alexander Reitzel <funtimecoding@gmail.com>
2018-01-28 02:49:16 +0100

    remove debug print

--- directory_tools/directory_tools.py
+++ directory_tools/directory_tools.py
@@ -79,7 +79,6 @@ class Commands:
             raise RuntimeError('Could not determine group number.')
 
         group_number += 1
-        print('New group number: ' + str(group_number))
 
         if not connection.add(
                 dn='cn=' + group_name + ',ou=groups,' + self.suffix,
