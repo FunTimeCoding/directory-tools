d2aa0c2c3d75e6b1761587af36f1ffe9feef645a
Alexander Reitzel <funtimecoding@gmail.com>
2018-02-03 20:07:15 +0100

    catch recipient refused exception

--- mail.py
+++ mail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from smtplib import SMTP
+from smtplib import SMTP, SMTPRecipientsRefused
 from email.message import EmailMessage
 
 message = EmailMessage()
@@ -8,10 +8,12 @@ message.set_content('content')
 message['Subject'] = 'subject'
 message['From'] = 'example@example.org'
 message['To'] = 'funtimecoding@gmail.com'
-transport = SMTP('localhost')
+transport = SMTP('m2')
 
 try:
     transport.send_message(message)
     transport.quit()
 except ConnectionRefusedError as exception:
+    print('Error: ' + str(exception))
+except SMTPRecipientsRefused as exception:
     print('Error: ' + str(exception))
