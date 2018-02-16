d083e331e3e8616ecc9320f7d712e9e14e0c629d
Alexander Reitzel <funtimecoding@gmail.com>
2018-02-17 00:36:56 +0100

    fix style concern with different id?

--- bin/delete.sh
+++ bin/delete.sh
@@ -4,5 +4,5 @@ DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}/../lib/directory-tools.sh"
-# shellcheck disable=SC2086
+# shellcheck disable=SC2068
 ${DELETE_MANAGER} $@
