7165cc7820193922f3483b30675f1a594eb81a9c
Alexander Reitzel <funtimecoding@gmail.com>
2018-02-17 01:09:54 +0100

    yaml is called pyyaml in python

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     keywords='slapd openldap abstraction command line web service',
     packages=['directory_tools'],
-    install_requires=['pyyaml', 'flask', 'ldap3', 'yaml'],
+    install_requires=['pyyaml', 'flask', 'ldap3', 'pyyaml'],
     python_requires='>=3.2',
     entry_points={
         'console_scripts': [
