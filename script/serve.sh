e22fb6fda80dad3431ffd812bde87cb46451e045
Alexander Reitzel <funtimecoding@gmail.com>
2020-05-11 20:32:42 +0200

    fix routes for wsgi with blueprint

--- script/serve.sh
+++ script/serve.sh
@@ -4,13 +4,6 @@ DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}" || exit 1; pwd)
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}/../lib/project.sh"
-id -u vagrant 2> /dev/null && VAGRANT_ENVIRONMENT='true' || VAGRANT_ENVIRONMENT='false'
-
-if [ "${VAGRANT_ENVIRONMENT}" = true ]; then
-    VIRTUAL_ENVIRONMENT_PATH='/home/vagrant/venv'
-else
-    VIRTUAL_ENVIRONMENT_PATH='.venv'
-fi
 
 if [ "${SYSTEM}" = Darwin ]; then
     SED='gsed'
@@ -20,6 +13,6 @@ fi
 
 UNDERSCORE=$(echo "${PROJECT_NAME}" | ${SED} --regexp-extended 's/-/_/g')
 # shellcheck source=/dev/null
-. "${VIRTUAL_ENVIRONMENT_PATH}/bin/activate"
-PORT=$(cat "${HOME}/.${PROJECT_NAME}.yaml" | shyaml get-value port)
+. "${HOME}/venv/bin/activate"
+PORT=$(shyaml get-value port < "${HOME}/.${PROJECT_NAME}.yaml")
 waitress-serve --port "${PORT}" --call "${UNDERSCORE}.application:create_app"
